032c8e72c2a59f5bbf76511b690e05fd4f1c475f
Murray Smith <murray@businessinstincts.ca>
2015-01-06 15:04:26 -0700

    Revert dca16c6..327b8b3
    This rolls back to commit dca16c63eb48fc5634724459a2db9ce62ce9be18.

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,6 +3,10 @@ window.coachella = angular.module('coachella', ['ui.bootstrap', 'firebase'])
 	.controller('AppCtrl', function($rootScope, $firebaseSimpleLogin){
 		$rootScope.$ref = new Firebase('https://coachellalp.firebaseio.com');
 		$rootScope.auth = $firebaseSimpleLogin($rootScope.$ref);
+		
+		$rootScope.$on('$firebaseSimpleLogin:login', function(e, user){
+			$rootScope.$ref.child('users/' + user.uid).set(user);
+		});
 	})
 	.controller('BandsCtrl', function($scope, $rootScope, $firebase) {
 		var bandsRef = $rootScope.$ref.child('bands');
@@ -11,6 +15,7 @@ window.coachella = angular.module('coachella', ['ui.bootstrap', 'firebase'])
 		
 		var usersRef = $rootScope.$ref.child('users');
 		$scope.users = $firebase(usersRef);
+		$scope.users.$bind($scope, 'users');
 		
 		$scope.vote = function(band_id, vote){
 			var save = function(){
@@ -32,16 +37,81 @@ window.coachella = angular.module('coachella', ['ui.bootstrap', 'firebase'])
 				save();
 			}
 		};
-		$scope.getScore = function(band_id){
-			var votes = $scope.bands[band_id].votes,
-				total = 0,
-				count = 0;
+		$scope.mustsee = function(band_id){
+			var save = function(){
+				var voteRef = $scope.bands.$child(band_id + '/mustsee/' + $rootScope.auth.user.uid);
+				
+				voteRef.$on('loaded', function(){
+					if(voteRef.$value){
+						voteRef.$remove();
+					} else {
+						voteRef.$set(true);
+					}
+				});
+			}
+			if ( ! $rootScope.auth.user){
+				$rootScope.auth.$login('facebook').then(function(user){
+					save();
+				}, function(error){
+					console.error(error);
+				});
+			}else{
+				save();
+			}
+		};
+		
+		$scope.orderBy     = undefined;
+		$scope.orderByStr  = undefined;
+		$scope.orderByDir  = undefined;
+		$scope.toggleOrder = function(key){
+			if($scope.orderByStr == key) $scope.orderByDir = ! $scope.orderByDir;
+			$scope.orderByStr = key;
+			switch(key){
+				case 'mustsee':
+					key = function(item){
+						var length = 0;
+						if(item.mustsee){
+							for(var i in item.mustsee) length++;
+						}
+						return length;
+					};
+					break;
+				case 'votes':
+					key = function(item){
+						var length = 0;
+						if(item.votes){
+							for(var i in item.votes) length++;
+						}
+						return length;
+					};
+					break;
+				case 'vote':
+					key = function(item){
+						var order = -2;
+						if(item.votes){
+							angular.forEach(item.votes, function(vote, user_uid){
+								if(user_uid == $rootScope.auth.user.uid) order = vote.vote;
+							});
+						}
+						return order;
+					};
+					break;
+			}
+			$scope.orderBy = key;
+			console.log($scope);
+		};
+		$scope.filterDay = 0;
+	})
+	.controller('BandCtrl', function($scope) {
+		$scope.init = function(band){
+			$scope.data = band;
+		};
+		$scope.$watch('data.votes', function(votes){
+			var total = 0;
 			angular.forEach(votes, function(item){
 				total += item.vote;
-				count++;
 			});
-			return count ? total / count : 0;
-		};
+			return $scope.data.score = total;
+		});
 	})
-	
 	;
